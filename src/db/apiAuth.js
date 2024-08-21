0e6c59499f444768ea67bc1dcaff6d817c43eb84
kenneth david <kennethdavid256.com>
2024-08-21 14:14:55 +0300

    scissor

--- src/db/apiAuth.js
+++ src/db/apiAuth.js
@@ -1,5 +1,6 @@
 import supabase, {supabaseUrl} from "./supabase";
 
+
 export async function login({email, password}) {
   const {data, error} = await supabase.auth.signInWithPassword({
     email,
--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,16 +1,7 @@
-// import {createClient} from "@supabase/supabase-js";
+import {createClient} from "@supabase/supabase-js";
 
-// export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-// const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-// const supabase = createClient(supabaseUrl, supabaseKey);
-
-// export default supabase;
-
-// src/db/supabase.js
-const { createClient } = require('@supabase/supabase-js');
-const supabaseUrl = process.env.VITE_SUPABASE_URL; // Update according to your environment
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY; // Update according to your environment
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-module.exports = { supabase };
-
+export default supabase;
