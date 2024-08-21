0faba9e7c8e4b327ed3c91417bf8cc7cacc4aecb
kenneth david <kennethdavid256.com>
2024-08-21 12:06:47 +0300

    scissor

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -12,13 +12,13 @@ const AppLayout = () => {
         {/* Made with 💗 by RoadsideCoder */}
         
   <a href="https://facebook.com/yourprofile" target="_blank" aria-label="Facebook">
-    <i class="fab fa-facebook"></i>
+    <i class="fab fa-facebook" className="h-24 w24"></i>
   </a>
   <a href="https://twitter.com/" target="_blank" aria-label="Twitter">
-    <i class="fab fa-twitter"></i>
+    <i class="fab fa-twitter" className="h-24 w24"></i>
   </a>
-  <a href="https://linkedin.com/in/yourprofile" target="_blank" aria-label="LinkedIn">
-  <i class="fab fa-linkedin"></i>
+  <a href="https://linkedin.com" target="_blank" aria-label="LinkedIn">
+  <i class="fab fa-linkedin" className="h-24 w24"></i>
   </a>
 </div>
 
